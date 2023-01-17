fb7813d3783a11a4f490491b648816d6c2eb1752
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2023-01-17 11:25:27 -0500

    chore: replace index.md with index.njk, add styling, include images/styles in 11ty config

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,14 @@
 module.exports = function (eleventyConfig) {
   // Set custom directories for input, output, includes, and data
+
+  // Copy `src/style.css` to `_site/style.css`
+  eleventyConfig.addPassthroughCopy('src/style.css');
+
+  eleventyConfig.addPassthroughCopy('src/images');
+
   return {
+    // When a passthrough file is modified, rebuild the pages:
+    passthroughFileCopy: true,
     dir: {
       input: 'src',
       includes: '_includes',
